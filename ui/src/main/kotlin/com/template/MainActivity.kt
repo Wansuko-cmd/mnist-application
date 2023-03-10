ad1cfc8a0a683ac36f545e80387c7123b9a448f6
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2023-03-11 00:41:55 +0900

    [add] CameraScreenを追加

--- ui/src/main/kotlin/com/template/MainActivity.kt
+++ ui/src/main/kotlin/com/template/MainActivity.kt
@@ -1,17 +1,35 @@
 package com.template
 
+import android.Manifest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.LaunchedEffect
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.PermissionRequired
+import com.google.accompanist.permissions.rememberPermissionState
 import com.template.theme.TemplateTheme
 import com.template.ui.MainScreen
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalPermissionsApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             TemplateTheme {
-                MainScreen()
+                val permissionState = rememberPermissionState(permission = Manifest.permission.CAMERA)
+
+                LaunchedEffect(Unit) {
+                    permissionState.launchPermissionRequest()
+                }
+
+                PermissionRequired(
+                    permissionState = permissionState,
+                    permissionNotGrantedContent = { /*TODO*/ },
+                    permissionNotAvailableContent = { /*TODO*/ },
+                ) {
+                    MainScreen()
+                }
             }
         }
     }
--- ui/src/main/kotlin/com/template/ui/MainScreen.kt
+++ ui/src/main/kotlin/com/template/ui/MainScreen.kt
@@ -1,9 +1,27 @@
 package com.template.ui
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.template.common.CameraScreen
+import java.nio.ByteBuffer
 
 @Composable
 fun MainScreen() {
-    Text(text = "Hello")
+    CameraScreen(
+        modifier = Modifier.fillMaxSize(),
+        analyze = { image ->
+            image.planes[0]
+                .buffer
+                .toByteArray()
+                .map { it.toInt() and 0xFF }
+        },
+    )
+}
+
+fun ByteBuffer.toByteArray(): ByteArray {
+    rewind() // Rewind the buffer to zero
+    val data = ByteArray(remaining())
+    get(data) // Copy the buffer into a byte array
+    return data // Return the byte array
 }
