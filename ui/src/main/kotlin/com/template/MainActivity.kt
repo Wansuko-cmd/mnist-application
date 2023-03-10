272030f2913ab7b235d53cfa6ad0ae50c4cea451
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2023-03-10 23:25:25 +0900

    [add] Composeの設定を追加

--- ui/src/main/kotlin/com/template/MainActivity.kt
+++ ui/src/main/kotlin/com/template/MainActivity.kt
@@ -1,5 +1,18 @@
 package com.template
 
+import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.template.theme.TemplateTheme
+import com.template.ui.MainScreen
 
-class MainActivity : ComponentActivity()
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TemplateTheme {
+                MainScreen()
+            }
+        }
+    }
+}
---
+++ ui/src/main/kotlin/com/template/theme/TemplateTheme.kt
@@ -0,0 +1,13 @@
+package com.template.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TemplateTheme(
+    content: @Composable () -> Unit,
+) {
+    MaterialTheme(
+        content = content,
+    )
+}
