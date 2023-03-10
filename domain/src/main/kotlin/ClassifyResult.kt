5c710be72184c16356c431c3f7b945217da17407
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2023-03-10 19:37:39 +0900

    [add] 分類結果を表すクラスを追加

---
+++ domain/src/main/kotlin/ClassifyResult.kt
@@ -0,0 +1,12 @@
+data class ClassifyResult(
+    val zero: Float,
+    val one: Float,
+    val two: Float,
+    val three: Float,
+    val four: Float,
+    val five: Float,
+    val six: Float,
+    val seven: Float,
+    val eight: Float,
+    val nine: Float,
+)
