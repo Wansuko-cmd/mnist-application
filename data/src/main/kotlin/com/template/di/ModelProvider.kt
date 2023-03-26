84c6fc0b93fac657738d9d7d5ca885be7874bc33
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2023-03-26 11:30:33 +0900

    [fix] Singletonアノテーションを追加

--- data/src/main/kotlin/com/template/di/ModelProvider.kt
+++ data/src/main/kotlin/com/template/di/ModelProvider.kt
@@ -7,11 +7,13 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 class ModelProvider {
     @Provides
+    @Singleton
     fun providesMnistModel(
         @ApplicationContext context: Context,
     ): MnistModel = MnistModel.newInstance(context)
